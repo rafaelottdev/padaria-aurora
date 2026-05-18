e5f0f768a31ead4aa4e52e35a94a297d56882eb8
Rafael Ott <157972006+rafaelottdev@users.noreply.github.com>
2026-05-18 13:38:56 -0300

    Category: add bootstrap

--- src/components/CategoryCard.tsx
+++ src/components/CategoryCard.tsx
@@ -11,18 +11,18 @@ type cardInfo = {
 function CategoryCard({ img, title, desc, price, tag }: cardInfo) {
     return (
         <li className={styles.card_container}>
-            <div className={styles.img_wrapp}>
-                <img src={img} />
+            <div className={`${styles.img_wrapp} w-100`}>
+                <img src={img} className="w-100 h-100" />
             </div>
 
-            <div className={styles.info_container}>
+            <div className={`${styles.info_container} d-flex flex-column`}>
                 <div className={styles.desc_container}>
                     <h3>{title}</h3>
 
                     <p>{desc}</p>
                 </div>
 
-                <div className={styles.price_container}>
+                <div className={`${styles.price_container} d-flex justify-content-between align-items-center`}>
                     <p>{price}</p>
 
                     <p>{tag}</p>
--- src/sections/Category/Category.tsx
+++ src/sections/Category/Category.tsx
@@ -6,26 +6,26 @@ function Category() {
     return (
         <section className={styles.category_section}>
             <div className={styles.category_header}>
-                <ul className={styles.category_list}>
+                <ul className={`${styles.category_list} d-flex justify-content-center`}>
                     <li>
-                        <NavLink to="/">Salgados</NavLink>
+                        <NavLink to="/" className="d-flex align-items-center justify-content-center text-decoration-none">Salgados</NavLink>
                     </li>
 
                     <li>
-                        <NavLink to="doces">Doces</NavLink>
+                        <NavLink to="doces" className="d-flex align-items-center justify-content-center text-decoration-none">Doces</NavLink>
                     </li>
 
                     <li>
-                        <NavLink to="bebidas">Bebidas</NavLink>
+                        <NavLink to="bebidas" className="d-flex align-items-center justify-content-center text-decoration-none">Bebidas</NavLink>
                     </li>
 
                     <li>
-                        <NavLink to="variedades">Variedades</NavLink>
+                        <NavLink to="variedades" className="d-flex align-items-center justify-content-center text-decoration-none">Variedades</NavLink>
                     </li>
                 </ul>
             </div>
 
-            <div className={styles.category_body}>
+            <div className={`${styles.category_body} d-flex flex-column align-items-center`}>
                 <Outlet />
             </div>
         </section>
@@ -34,4 +34,4 @@ function Category() {
 
 export default Category
 
-// Adicionar o bootstrap e deixar as imagens menos pesadas
+// Adicionar o bootstrap e deixar as imagens menos pesadas, fundo do site (melhorar), ver porque o fundo dos container n ta aparecendo no mobile
